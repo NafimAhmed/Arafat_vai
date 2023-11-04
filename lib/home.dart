7388c96643cb5ef4729c4eddddb4f744b7b42266
NafimAhmed <recentnafimahmed@gmail.com>
2023-11-05 00:12:14 +0600

    Day 3.3

--- lib/home.dart
+++ lib/home.dart
@@ -26,183 +26,192 @@ class _HomeState extends State<Home> {
     // TODO: implement build
     return Scaffold(
       backgroundColor: Colors.green,
-      body: Container(
-        width: 100.w,
-        height: 100.h,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
+      body: SingleChildScrollView(
+        child: Container(
+          width: 100.w,
+          height: 100.h,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
 
-            Container(
-              width: 80.w,
-              height: 60.h,
-              decoration: BoxDecoration(
-                color: Colors.white,
+              Container(
+                width: 80.w,
+                height: 60.h,
+                decoration: BoxDecoration(
+                  color: Colors.white,
 
-                borderRadius: BorderRadius.circular(10)
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Text("Select",
+                            style: GoogleFonts.inter(
+                              fontSize: 18.sp,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.green
+                            ),
+                          ),
+
+                        ],
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
+                      child: Row(
+                        children: [
+                          Text("Your option before start",
+                            style: GoogleFonts.inter(
+                              fontSize: 12,
+                              color: Colors.green
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text("Select",
-                          style: GoogleFonts.inter(
-                            fontSize: 18.sp,
+
+                        Icon(Icons.translate),
+
+                        Container(
+                          margin: EdgeInsets.all(10),
+                          padding: EdgeInsets.all(10),
+                          width: 60.w,
+                          height: 40,
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Colors.transparent,
+                                  width: 0
+                              )
+                          ),
+
+
+                          child: DropdownButton<String>(
+                            isExpanded: true,
+                            hint: Text("Choose Your Language"),
+                            value: dropdownValue,
+                            icon: const Icon(Icons.arrow_drop_down),
+                            elevation: 16,
+                            style: const TextStyle(color: Colors.deepPurple),
+                            underline: Container(
+                              height: 2,
+                              color: Colors.transparent,
+                            ),
+
+
+
+                            onChanged: (String? value) {
+                              // This is called when the user selects an item.
+                              setState(() {
+                                dropdownValue = value!;
+                              });
+                            },
+                            items: list.map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                          ),
+                        ),
+                      ],
+                    ),
+
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+
+                        Icon(Icons.euro_outlined),
+
+
+                        Container(
+                          margin: EdgeInsets.all(10),
+                          padding: EdgeInsets.all(10),
+                          width: 60.w,
+                          height: 40,
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Colors.transparent,
+                                  width: 0
+                              )
+                          ),
+
+
+                          child: DropdownButton<String>(
+                            hint: Text("Choose Currency"),
+                            isExpanded: true,
+                            value: dropdownValue1,
+                            icon: const Icon(Icons.arrow_drop_down),
+                            elevation: 16,
+                            style: const TextStyle(color: Colors.deepPurple),
+                            underline: Container(
+                              height: 2,
+                              color: Colors.transparent,
+                            ),
+
+
+
+                            onChanged: (String? value) {
+                              // This is called when the user selects an item.
+                              setState(() {
+                                dropdownValue1 = value!;
+                              });
+                            },
+                            items: list1.map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                          ),
+                        ),
+                      ],
+                    ),
+
+
+
+                    InkWell(
+                      onTap: (){
+                        Get.to(SignInPage());
+                      },
+                      child: Container(
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.green
+                        ),
+                        height: 50,
+                        width: 70.w,
+                        child: Text("Continue",
+
+                          style: GoogleFonts.poppins(
+                            fontSize: 16.sp,
                             fontWeight: FontWeight.bold,
-                            color: Colors.green
+                            color: Colors.white
                           ),
-                        ),
-
-                      ],
-                    ),
-                  ),
-
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
-                    child: Row(
-                      children: [
-                        Text("Your option before start",
-                          style: GoogleFonts.inter(
-                            fontSize: 12,
-                            color: Colors.green
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-
-                      Icon(Icons.translate),
-
-                      Container(
-                        margin: EdgeInsets.all(10),
-                        padding: EdgeInsets.all(10),
-                        width: 60.w,
-                        height: 40,
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Colors.transparent,
-                                width: 0
-                            )
-                        ),
-
-
-                        child: DropdownButton<String>(
-                          isExpanded: true,
-                          hint: Text("Choose Your Language"),
-                          value: dropdownValue,
-                          icon: const Icon(Icons.arrow_drop_down),
-                          elevation: 16,
-                          style: const TextStyle(color: Colors.deepPurple),
-                          underline: Container(
-                            height: 2,
-                            color: Colors.transparent,
-                          ),
-
-
-
-                          onChanged: (String? value) {
-                            // This is called when the user selects an item.
-                            setState(() {
-                              dropdownValue = value!;
-                            });
-                          },
-                          items: list.map<DropdownMenuItem<String>>((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value),
-                            );
-                          }).toList(),
                         ),
                       ),
-                    ],
-                  ),
-
-
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-
-                      Icon(Icons.euro_outlined),
-
-
-                      Container(
-                        margin: EdgeInsets.all(10),
-                        padding: EdgeInsets.all(10),
-                        width: 60.w,
-                        height: 40,
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Colors.transparent,
-                                width: 0
-                            )
-                        ),
-
-
-                        child: DropdownButton<String>(
-                          hint: Text("Choose Currency"),
-                          isExpanded: true,
-                          value: dropdownValue1,
-                          icon: const Icon(Icons.arrow_drop_down),
-                          elevation: 16,
-                          style: const TextStyle(color: Colors.deepPurple),
-                          underline: Container(
-                            height: 2,
-                            color: Colors.transparent,
-                          ),
+                    )
 
 
 
-                          onChanged: (String? value) {
-                            // This is called when the user selects an item.
-                            setState(() {
-                              dropdownValue1 = value!;
-                            });
-                          },
-                          items: list1.map<DropdownMenuItem<String>>((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value),
-                            );
-                          }).toList(),
-                        ),
-                      ),
-                    ],
-                  ),
+                  ],
+                ),
+              )
 
-
-
-                  InkWell(
-                    onTap: (){
-                      Get.to(SignInPage());
-                    },
-                    child: Container(
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.green
-                      ),
-                      height: 40,
-                      width: 80.w,
-                      child: Text("Continue"),
-                    ),
-                  )
-
-
-
-                ],
-              ),
-            )
-
-          ],
+            ],
+          ),
         ),
       )
     );
--- lib/sign_in_page.dart
+++ lib/sign_in_page.dart
@@ -3,6 +3,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sizer/sizer.dart';
 
 class SignInPage extends StatelessWidget{
@@ -11,113 +12,186 @@ class SignInPage extends StatelessWidget{
     // TODO: implement build
     return Scaffold(
       backgroundColor: Colors.green,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
+      body: SingleChildScrollView(
+        child: Container(
+          width: 100.w,
+          height: 100.h,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
 
-
-          Container(
-
-
-            width:70.w,
-            //padding: EdgeInsets.symmetric(horizontal: 16,vertical: 16),
-            child: TextField(
-
-              // controller: passwordController,
-              // obscureText: _isObscure,
-              keyboardType: TextInputType.text,
-              cursorColor: Colors.red.shade900,
-              decoration: InputDecoration(
-
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                child: Row(
+                  children: [
+                    Text("Sign in",
+                      style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.white
+                      ),
+                    ),
+                  ],
                 ),
-
-
-                // border: InputBorder.none,
-                // focusedBorder: InputBorder.none,
-                // enabledBorder: InputBorder.none,
-                // errorBorder: InputBorder.none,
-                // disabledBorder: InputBorder.none,
-
-                fillColor: Colors.white,
-                filled: true,
-                prefixIcon: Icon(Icons.dialpad,size: 30,color: Colors.green,),
-                // suffixIcon: IconButton(
-                //   icon: Icon(_isObscure ? Icons.visibility : Icons.visibility_off),
-                //   onPressed: (){
-                //     setState(() {
-                //       _isObscure= !_isObscure;
-                //     });
-                //   },
-                // ),
-
-                suffixIconColor: Colors.red,
-                // border: OutlineInputBorder(
-                //     borderRadius: BorderRadius.circular(4),
-                //     borderSide: BorderSide(width: 1,color: Colors.transparent)
-                //
-                // ),
-
-
-                hintText: "Whats app Number",
-
-                // focusedBorder: OutlineInputBorder(
-                //     borderRadius: BorderRadius.circular(4),
-                //     borderSide: BorderSide(
-                //         color: Colors.transparent,
-                //         width: 1.0
-                //     )
-                // )
               ),
-            ),
-          ),
-
-
-
-          OtpTextField(
-            fieldWidth: 50,
-            numberOfFields: 4,
-            borderColor: Color(0xFF512DA8),
-            //set to true to show as box or false to show as dash
-            showFieldAsBox: true,
-            //runs when a code is typed in
-            onCodeChanged: (String code) {
-              //handle validation or checks here
-            },
-            //runs when every textfield is filled
-            onSubmit: (String verificationCode){
-              showDialog(
-                  context: context,
-                  builder: (context){
-                    return AlertDialog(
-                      title: Text("Verification Code"),
-                      content: Text('Code entered is $verificationCode'),
-                    );
-                  }
-              );
-            }, // end onSubmit
-          ),
-
-
-
-          InkWell(
-            onTap: (){
-              //Get.to(SignInPage());
-            },
-            child: Container(
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.white
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+                child: Row(
+                  children: [
+                    Text("Sign in to continue",
+                      style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12,
+                          color: Colors.white
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              height: 40,
-              width: 80.w,
-              child: Text("Sign in"),
-            ),
-          )
+
+
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: Row(
+                  children: [
+                    Text("Your whatsapp number",
+                      style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12,
+                          color: Colors.white
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+
+
+
+
+              Container(
+
+
+                width:90.w,
+                //padding: EdgeInsets.symmetric(horizontal: 16,vertical: 16),
+                child: TextField(
+
+                  // controller: passwordController,
+                  // obscureText: _isObscure,
+                  keyboardType: TextInputType.text,
+                  cursorColor: Colors.red.shade900,
+                  decoration: InputDecoration(
+
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+
+                    ),
+
+
+                    // border: InputBorder.none,
+                    // focusedBorder: InputBorder.none,
+                    // enabledBorder: InputBorder.none,
+                    // errorBorder: InputBorder.none,
+                    // disabledBorder: InputBorder.none,
+
+                    fillColor: Colors.white,
+                    filled: true,
+                    prefixIcon: Icon(Icons.dialpad,size: 30,color: Colors.green,),
+                    // suffixIcon: IconButton(
+                    //   icon: Icon(_isObscure ? Icons.visibility : Icons.visibility_off),
+                    //   onPressed: (){
+                    //     setState(() {
+                    //       _isObscure= !_isObscure;
+                    //     });
+                    //   },
+                    // ),
+
+                    suffixIconColor: Colors.red,
+                    // border: OutlineInputBorder(
+                    //     borderRadius: BorderRadius.circular(4),
+                    //     borderSide: BorderSide(width: 1,color: Colors.transparent)
+                    //
+                    // ),
+
+
+                    hintText: "Whats app Number",
+
+                    // focusedBorder: OutlineInputBorder(
+                    //     borderRadius: BorderRadius.circular(4),
+                    //     borderSide: BorderSide(
+                    //         color: Colors.transparent,
+                    //         width: 1.0
+                    //     )
+                    // )
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: 20,
+              ),
+
+              OtpTextField(
+                fieldWidth: 50,
+                numberOfFields: 4,
+                borderColor: Color(0xFF512DA8),
+                //set to true to show as box or false to show as dash
+                showFieldAsBox: true,
+                //runs when a code is typed in
+                onCodeChanged: (String code) {
+                  //handle validation or checks here
+                },
+                //runs when every textfield is filled
+                onSubmit: (String verificationCode){
+                  showDialog(
+                      context: context,
+                      builder: (context){
+                        return AlertDialog(
+                          title: Text("Verification Code"),
+                          content: Text('Code entered is $verificationCode'),
+                        );
+                      }
+                  );
+                }, // end onSubmit
+              ),
+
+
+
+
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+                child: Row(
+                  children: [
+                    Text("Your whatsapp number",
+                      style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12,
+                          color: Colors.white
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+
+
+
+              InkWell(
+                onTap: (){
+                  //Get.to(SignInPage());
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white
+                  ),
+                  height: 40,
+                  width: 80.w,
+                  child: Text("Sign in"),
+                ),
+              )
 
 
 
@@ -126,7 +200,9 @@ class SignInPage extends StatelessWidget{
 
 
 
-        ],
+            ],
+          ),
+        ),
       ),
     );
   }
